6dd15b2d2082678b1552a243a1819f17daed48b9
Vikash <vikaspal@deloitte.com>
2025-07-30 17:48:28 +0530

    Added github Actions workflow

--- test/features/demo/alerts.feature
+++
@@ -1,6 +0,0 @@
-Feature:Alerts
-
-Scenario Outline: ALerts and pop ups
-
-Given Open the url
-When Click on here 
--- test/features/step-definition/demo.ts
+++ test/features/step-definition/demo.ts
@@ -30,55 +30,54 @@ Then('I should be logged in successfully',async function() {
 })
 
 
-// Given('web is opened',async function() {
-//   await browser.url("https://rahulshettyacademy.com/AutomationPractice/");
-//   await browser.setTimeout({implicit:15000,pageLoad:10000});
-//   await browser.maximizeWindow();
+Given('web is opened',async function() {
+  await browser.url("https://rahulshettyacademy.com/AutomationPractice/");
+  await browser.setTimeout({implicit:15000,pageLoad:10000});
+  await browser.maximizeWindow();
 
-// })
+})
 
-// When('perform web Interactions',async function() {
-//  let radioBtn=await $(`input[value='radio1']`);
-//  await radioBtn.click();
+When('perform web Interactions',async function() {
+ let radioBtn=await $(`input[value='radio1']`);
+ await radioBtn.click();
 
-//  let dropdown= await $(`#dropdown-class-example`);
-// //  await dropdown.selectByIndex(1);
-//  await dropdown.selectByAttribute('value','option2')
-//  await browser.pause(10000); 
-// })
+ let dropdown= await $(`#dropdown-class-example`);
+//  await dropdown.selectByIndex(1);
+ await dropdown.selectByAttribute('value','option2')
+ await browser.pause(10000); 
+})
 
 
 /*Window Handling
 */
 
-// Given('Open the url',async function(){
-//   await browser.url("https://the-internet.herokuapp.com/windows");
-//   await browser.setTimeout({implicit:15000,pageLoad:10000});
-//   await browser.maximizeWindow();
-// })
+Given('Open the url',async function(){
+  await browser.url("https://the-internet.herokuapp.com/windows");
+  await browser.setTimeout({implicit:15000,pageLoad:10000});
+  await browser.maximizeWindow();
+})
 
-// When('Click on here to open new window',async()=>{
-//   console.log(`>>>>Entered in the window handle`);
-//   let clickBtn= await $(`a=Click Here`);
-//   let elementBtn=await $(`a=Elemental Selenium`);  
-//   clickBtn.click();
-//   elementBtn.click();
-//   let currentTitle=await browser.getTitle();
-//   console.log(`>> current window title:${currentTitle}`);
+When('Click on here to open new window',async()=>{
+  console.log(`>>>>Entered in the window handle`);
+  let clickBtn= await $(`a=Click Here`);
+  let elementBtn=await $(`a=Elemental Selenium`);  
+  clickBtn.click();
+  elementBtn.click();
+  let currentTitle=await browser.getTitle();
+  console.log(`>> current window title:${currentTitle}`);
 
-//   //switch to specific window
-//   let windowHandles=await browser.getWindowHandles();
-//   for(let i=0; i<windowHandles.length; i++){
-//     console.log(`>> window handle ${windowHandles[i]}`);
-//     await browser.switchToWindow(windowHandles[i]);
-//     currentTitle=await browser.getTitle();
-//     if(currentTitle==="Home | Elemental Selenium"){
-//       await browser.switchToWindow(windowHandles[i]);
-//       break;
-//   }
-
-
-// }
+  //switch to specific window
+  let windowHandles=await browser.getWindowHandles();
+  for(let i=0; i<windowHandles.length; i++){
+    console.log(`>> window handle ${windowHandles[i]}`);
+    await browser.switchToWindow(windowHandles[i]);
+    currentTitle=await browser.getTitle();
+    if(currentTitle==="Home | Elemental Selenium"){
+      await browser.switchToWindow(windowHandles[i]);
+      break;
+  }
+}
+})
 
  //alerts
 
@@ -111,11 +110,11 @@ Then('I should be logged in successfully',async function() {
 //   await browser.maximizeWindow();
 // });
 
-// When('Click on here',async()=>{
+// When('Click on JS Alerts',async()=>{
 //   await $(`button=Click for JS Alert`).click();
 // if(await browser.isAlertOpen()){
 //    await browser.acceptAlert();
-//   console.log(await $(`#result`).getText());
+//   // console.log(await $(`#result`).getText());
 // }
 //   browser.pause(1000);
 
